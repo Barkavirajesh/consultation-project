5e215e344ecd93b9b6c7faa86d138290480ed83c
Barkavirajesh <barkavir.cse2024@citchennai.net>
2025-11-14 18:32:42 +0530

    first commit

--- backend/server.js
+++ backend/server.js
@@ -381,8 +381,8 @@ app.get("/consultation/:id", (req, res) => {
 app.use(express.static(path.join(__dirname, "../frontend/build")));
 
 // Catch-all for React routing
-app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "../frontend/build", "index.html"));
+app.use((req, res) => {
+    res.sendFile(path.join(__dirname, "build", "index.html"));
 });
 
 // Start Server
