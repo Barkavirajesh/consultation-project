164820b2fc16c60fa81f093c545f827da80635cf
Barkavirajesh <barkavir.cse2024@citchennai.net>
2025-11-22 15:57:12 +0530

    i want to deploy

--- backend/server.js
+++ backend/server.js
@@ -5,6 +5,7 @@ const express = require("express");
 const nodemailer = require("nodemailer");
 const cors = require("cors");
 const { v4: uuidv4 } = require("uuid");
+const FRONTEND_URL = "https://tch-chennai.onrender.com";
 
 const app = express();
 app.use(cors());
